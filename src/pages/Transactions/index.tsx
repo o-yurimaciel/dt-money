bbf293e714b6ee00506dace03fcf04ecce1322f7
Yuri Maciel <yurimpro@outlook.com>
2023-12-14 17:59:26 -0300

    add transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionsContainer, TransactionsTable, PriceHighlight } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Type</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000
+                                </PriceHighlight>
+                            </td>
+                            <td>Category</td>
+                            <td>Date</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Type</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                    - R$ 569,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Category</td>
+                            <td>Date</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
