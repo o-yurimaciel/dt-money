256b9dec7fb324803a0f59f70ecb0567c18568e5
Yuri Maciel <yurimpro@outlook.com>
2024-01-09 10:29:24 -0300

    add new transaction modal with radix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,20 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logo from '../../assets/dt-money-logo.svg'
+import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
                 <img src={logo} alt="" />
-                <NewTransactionButton>New transaction</NewTransactionButton>
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>New transaction</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <NewTransactionModal />
+                </Dialog.Root>
             </HeaderContent>
         </HeaderContainer>
     )
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,26 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { Overlay, Content, CloseButton } from './styles'
+import { X } from 'phosphor-react'
+
+export function NewTransactionModal() {
+    return (
+        <Dialog.Portal>
+            <Overlay />
+            <Content>
+                <Dialog.Title>New transaction</Dialog.Title>
+                CloseButton
+                <CloseButton>
+                    <X size={24}/>
+                </CloseButton>
+
+                <form action="">
+                    <input type="text" placeholder='Description' required />
+                    <input type="number" placeholder='Price' required />
+                    <input type="text" placeholder='Category' required />
+
+                    <button type="submit">Register</button>
+                </form>
+            </Content>
+        </Dialog.Portal>
+    )
+}
